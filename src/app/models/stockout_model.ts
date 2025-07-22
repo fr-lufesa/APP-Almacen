7e99b2ed59720fd4b43b0c51840aabdeb8a59fc4
Francisco Reyna <francisco.reyna@alumnos.udg.mx>
2025-07-22 11:05:27 -0600

    Fix: PPTO STOCKOUT optional

--- src/app/models/stockout_model.ts
+++ src/app/models/stockout_model.ts
@@ -1,8 +1,9 @@
 export interface StockoutRequest{
     idProducto: number,
     cantidad: number
-    ppto: string,
+    ppto?: string,
     fecha: Date
     costoUnitario?: number
+    // saco_blanco?: boolean
     // usuario: string  
 }
